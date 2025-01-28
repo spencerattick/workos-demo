583a2fd542d48d983045acb75d4c3441eee472c2
Spencer Attick <spencer.attick@gmail.com>
2025-01-28 06:34:41 -0500

    begin to add styling

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 // [ ] double check logout functionality (might not be working right)
-// [ ] maintain firstname/lastname when switching from directory page back to the loggedIn page
 // [ ] ensure all /api files are using try/catch
 // [ ] FIGURE OUT WHY USERS AREN'T ADDED TO GROUP WITH TEST SSO
+// [ ] Make sure EVERYTHING works without localhost
+// [ ] fill out readme
+// [ ] figure out how to log into the app if user clicks the tile in Okta
 // [ ] choose semi colons or no semi colons
-// import '../styles/index.css'
+import styles from '../styles/index.module.css';
 
 export default function Home() {
   const handleSignInOktaButtonClick = () => {
@@ -15,7 +17,7 @@ export default function Home() {
   }
 
   return (
-    <div>
+    <div className={styles.welcomePage}>
       <h1>Welcome! Please sign in!</h1>
       <button onClick={handleSignInOktaButtonClick}>SIGN IN WITH OKTA</button>
       <button onClick={handleSignInTestButtonClick}>SIGN IN WITH TEST SSO</button>
